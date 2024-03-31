2eb1068960fffbf0cccaaa1b3dc6283ab576f1a1
jiangwan <jiangwan8273@163.com>
2024-03-31 16:32:41 +0800

    [improve]  添加type

--- packages/directive/vite.config.js
+++ packages/directive/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import { buildPlugin } from "vite-plugin-build";
 
 export default defineConfig({
-  build: {
-    target: "es2015",
-  },
   plugins: [
     buildPlugin({
       fileBuild: { inputFolder: "utils", emitDeclaration: true },
